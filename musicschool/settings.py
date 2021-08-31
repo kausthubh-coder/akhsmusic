8b43b3d7b247b1981e6c823bc6256590fb017b73
kausthubh-coder <kausthubh2007@gmail.com>
2021-08-31 13:37:39 -0400

    first commit

--- musicschool/settings.py
+++ musicschool/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'e^fe#6c0^h146nkj#*$%xd&8=ja2xaow6*uetenm#j$^xzz+-$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
